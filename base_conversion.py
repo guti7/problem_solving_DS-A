65af82b0ad9066e3891637a0ca2dfceea651532b
guti7 <7505855+guti7@users.noreply.github.com>
2018-04-19 15:14:44 -0700

    Convert decimal to any base, string conversion

--- base_conversion.py
+++ base_conversion.py
@@ -16,6 +16,25 @@ def divide_by_2(number):
         
     return convertedString
 
+def baseConverter(decimal, base):
+    digits = "0123456789ABCDEF"
+    
+    converted = Stack()
+    
+    if (decimal == 0) :
+        converted.push(0)
+        
+    while decimal > 0:
+        digit = decimal % base
+        converted.push(digits[digit])
+        decimal /= base
+        
+    convertedString = ""
+    while not converted.isEmpty():
+        convertedString += converted.pop()
+    
+    return convertedString
+    
 
 def main():
     
@@ -42,6 +61,26 @@ def main():
     numberToConvert = 0
     convertedToBinary = divide_by_2(numberToConvert)
     print("number: %3i, base2: %10s" %(numberToConvert, convertedToBinary))
+    
+    numberToConvert = 233
+    convertedToBinary = baseConverter(numberToConvert, 8)
+    print("number: %3i, base%2i: %10s" %(numberToConvert, 8, convertedToBinary))
+    convertedToBinary = baseConverter(numberToConvert, 16)
+    print("number: %3i, base%2i: %10s" %(numberToConvert, 16, convertedToBinary))
+    
+    numberToConvert = 10
+    convertedToBinary = baseConverter(numberToConvert, 8)
+    print("number: %3i, base%2i: %10s" %(numberToConvert, 8, convertedToBinary))
+    convertedToBinary = baseConverter(numberToConvert, 16)
+    print("number: %3i, base%2i: %10s" %(numberToConvert, 16, convertedToBinary))
+    
+    numberToConvert = 25
+    convertedToBinary = baseConverter(numberToConvert, 2)
+    print("number: %3i, base%2i: %10s" %(numberToConvert, 2, convertedToBinary))
+    convertedToBinary = baseConverter(numberToConvert, 8)
+    print("number: %3i, base%2i: %10s" %(numberToConvert, 8, convertedToBinary))
+    convertedToBinary = baseConverter(numberToConvert, 16)
+    print("number: %3i, base%2i: %10s" %(numberToConvert, 16, convertedToBinary))
     
 
 if __name__ == '__main__':
